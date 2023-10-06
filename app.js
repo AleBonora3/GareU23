8682f209c40692340680c8d8287f380c772eb34b
AleBonora3 <alebonora3@users.noreply.github.com>
2023-10-06 10:48:30 +0200

    Aggiornamenti per mongoDB

--- app.js
+++ app.js
@@ -71,9 +71,36 @@ mongoose.connect('mongodb://localhost/GareDB', {
 // con l'uso delle opzioni useNewUrlParser (implicitamente impostato su true) e 
 // useUnifiedTopology: true per garantire una connessione robusta e moderna.
 
+mongoose.connect('mongodb+srv://AlessioBonora:Bonny123!@cluster0.n2ggedi.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://AlessioBonora:Bonny123!@cluster0.n2ggedi.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 module.exports = app;
