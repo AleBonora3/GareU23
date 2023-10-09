545768cfb8e5316ca33f0719cdcc11df68223fd7
AleBonora3 <alebonora3@users.noreply.github.com>
2023-10-09 14:17:20 +0200

    Provo a committare node-fetch

--- routes/percorsi.js
+++ routes/percorsi.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-import fetch from 'node-fetch'; //per supportare la fetch con la versione del servizio di host
+const fetch = require('node-fetch').default;//per supportare la fetch con la versione del servizio di host
 
 const gpxParse = require('gpx-parse');
 //per calcolare le distanze dati latitudine e longitudine
